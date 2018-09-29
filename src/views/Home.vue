6c29ad0692e1128037e050b6ce2449cbba2ff4ab
Fabio Teixeira <fabio1079@gmail.com>
2018-09-29 10:22:54 -0300

    Add simple map

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,66 +1,91 @@
 <template>
-  <div class="container">
-    <h1>Modo atual</h1>
-
-    <hr />
-
-    <div class="form-check form-check-inline">
-      <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" @click="setMode('none')" :checked="currentMode == 'none'">
-      <label class="form-check-label" for="exampleRadios1">
-        Desabilitado
-      </label>
+  <div id="main-container" class="container">
+    <div id="controls" class="row">
+      Top
     </div>
 
-    <div class="form-check form-check-inline">
-      <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2" @click="setMode('fixed')" :checked="currentMode == 'fixed'">
-      <label class="form-check-label" for="exampleRadios2">
-        Posição fixa
-      </label>
+    <div id="views" class="row">
+      <div id="cam-container" class="col col-6">Left</div>
+      <div id="map-container" class="col col-6">
+        <div id="map-tracker"></div>
+      </div>
     </div>
-
-    <div class="form-check disabled form-check-inline">
-      <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3" @click="setMode('track')" :checked="currentMode == 'track'">
-      <label class="form-check-label" for="exampleRadios3">
-        Rastreador de satélite
-      </label>
-    </div>
-
-    <hr />
-
-    <div>
-      <NotSelected v-if="currentMode=='none'" />
-      <FixedPosition v-if="currentMode=='fixed'" />
-      <TrackSatellite v-if="currentMode=='track'" />
-    </div>
-
-    <hr />
   </div>
 </template>
 
 <script>
-import FixedPosition from "@/components/FixedPosition";
-import NotSelected from "@/components/NotSelected";
-import TrackSatellite from "@/components/TrackSatellite";
-
 export default {
   name: "home",
 
-  components: {
-    FixedPosition,
-    NotSelected,
-    TrackSatellite
-  },
+  components: {},
 
   data() {
-    return {
-      currentMode: "track"
-    };
+    return {};
+  },
+
+  mounted() {
+    this.initMap();
   },
 
   methods: {
-    setMode(mode) {
-      this.currentMode = mode;
+    initMap() {
+      let map = L.map("map-tracker");
+      let satIcon = L.icon({
+        iconUrl: "/images/sate-1.png",
+        iconSize: [60, 60]
+      });
+
+      let lat = -15.77972;
+      let long = -47.92972;
+
+      map.setView([lat, long], 3);
+
+      let marker = L.marker([lat, long], { icon: satIcon });
+      marker.addTo(map);
+
+      const URI =
+        "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw";
+
+      L.tileLayer(URI, {
+        maxZoom: 18,
+        attribution: `
+                  Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>
+                  contributors, 
+                    <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>,
+                  Imagery © <a href="https://www.mapbox.com/">Mapbox</a>
+                `,
+        id: "mapbox.streets"
+      }).addTo(map);
     }
   }
 };
 </script>
+
+<style>
+#main-container {
+  background-color: gray;
+  border: solid 2px #000;
+  height: 99vh;
+}
+
+#controls {
+  background-color: green;
+  height: 40%;
+}
+
+#views {
+  height: 60%;
+}
+
+#cam-container {
+  background-color: gray;
+}
+#map-container {
+  background-color: blue;
+}
+#map-tracker {
+  background-color: yellow;
+  width: 100%;
+  height: 100%;
+}
+</style>
