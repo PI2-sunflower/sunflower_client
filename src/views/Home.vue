da2299de86d7c39180df680416ac6e3599fbc260
Fabio Teixeira <fabio1079@gmail.com>
2018-09-29 12:10:21 -0300

    Track satellite on the map

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,6 +15,17 @@
 
 <script>
 import { getMap, getMarker } from "../services/track-map";
+import axios from "../services/axios-setup";
+
+const MapData = {
+  map: null,
+  marker: null,
+  from: null,
+  to: null,
+  positions: []
+};
+
+window.MapData = MapData;
 
 export default {
   name: "home",
@@ -27,6 +38,7 @@ export default {
 
   mounted() {
     this.initMap();
+    this.getPositions();
   },
 
   methods: {
@@ -35,7 +47,55 @@ export default {
       let long = -47.92972;
 
       const map = getMap("map-tracker", lat, long);
-      const marker = getMarker(map, lat, long);
+      MapData.map = map;
+    },
+
+    async getPositions() {
+      let { data, status } = await axios.get("/track-data");
+
+      if (data.positions) {
+        let { from, to } = data.positions_validation;
+
+        MapData.from = new Date(from);
+        MapData.to = new Date(to);
+        MapData.positions = data.positions;
+
+        this.setMarker();
+      }
+    },
+
+    setMarker() {
+      let curPosition = 0;
+      let { satlatitude, satlongitude } = MapData.positions[0];
+
+      if (MapData.marker === null) {
+        const marker = getMarker(MapData.map, satlatitude, satlongitude);
+        MapData.marker = marker;
+      } else {
+        MapData.marker.addTo(MapData.map);
+      }
+
+      MapData.map.setView([satlatitude, satlongitude], 3);
+
+      let self = this;
+
+      function tracker() {
+        let position = MapData.positions[curPosition++];
+        let { satlatitude, satlongitude } = position;
+
+        MapData.marker.setLatLng([satlatitude, satlongitude]);
+
+        MapData.from.setSeconds(MapData.from.getSeconds() + 1);
+
+        if (MapData.from < MapData.to) {
+          window.setTimeout(tracker, 1000);
+        } else {
+          MapData.marker.remove();
+          self.getPositions();
+        }
+      }
+
+      tracker();
     }
   }
 };
