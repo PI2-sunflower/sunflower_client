0bde3601b227a766ba9c614a1622cd0d7fff3d29
Fabio Teixeira <fabio1079@gmail.com>
2018-10-02 11:54:30 -0300

    Add Satellite tracking interface control

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,75 @@
 <template>
   <div id="main-container" class="container">
     <div id="controls" class="row">
-      Top
-    </div>
+      <div class="col col-12">
+        <ul class="nav nav-pills">
+          <li class="nav-item">
+            <span class="nav-link disabled">Modo: </span>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Rastreador</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Fixo</a>
+          </li>
+        </ul>
+
+        <hr />
+
+        <div class="row">
+          <div class="col col-6">
+            <div id="tracker-form">
+              <div class="form-group">
+                <label for="satNORD">Código NORD</label>
+                <input
+              type="text"
+              class="form-control"
+              id="satNORD"
+              aria-describedby="satNORD"
+              placeholder="000000" v-model="NORD">
+                <small id="satNORDHelp" class="form-text text-muted">
+                  Não sabe o código NORD ?
+                  <a href="https://www.n2yo.com/database/" target="BLANK">Clique aqui</a>
+                </small>
+              </div><!-- /form-group -->
+
+              <button v-if="isTracking === false" class="btn btn-primary" @click="setTracking(true)">Rastrear</button>
+              <button v-if="isTracking === true" class="btn btn-danger" @click="setTracking(false)">Parar rastreamento</button>
+            </div><!-- /#tracker-form -->
+          </div><!-- /col-6 -->
+
+          <div class="col col-6">
+            <table class="table table-striped">
+              <tbody>
+                <tr>
+                  <td>Name:</td>
+                  <td>{{current.satname}}</td>
+                </tr>
+                <tr>
+                  <td>Latidude:</td>
+                  <td>{{current.satlatitude}}</td>
+                </tr>
+                <tr>
+                  <td>Longitude:</td>
+                  <td>{{current.satlongitude}}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div><!-- /col-6 -->
+        </div><!-- /row -->
+      </div><!-- /col-12 -->
+    </div><!-- /row -->
 
     <div id="views" class="row">
-      <div id="cam-container" class="col col-6">Left</div>
+      <div id="cam-container" class="col col-6">
+        Video da câmera
+      </div><!-- /col-6 -->
+
       <div id="map-container" class="col col-6">
         <div id="map-tracker"></div>
-      </div>
-    </div>
-  </div>
+      </div><!-- /col-6 -->
+    </div><!-- /row -->
+  </div><!-- /container -->
 </template>
 
 <script>
@@ -22,7 +81,8 @@ const MapData = {
   marker: null,
   from: null,
   to: null,
-  positions: []
+  positions: [],
+  info: null
 };
 
 window.MapData = MapData;
@@ -33,15 +93,50 @@ export default {
   components: {},
 
   data() {
-    return {};
+    return {
+      NORD: "25544",
+      isTracking: false,
+      current: {}
+    };
+  },
+
+  watch: {
+    NORD() {
+      if (this.isTracking) {
+        this.setTracking(false);
+      }
+    }
   },
 
   mounted() {
     this.initMap();
-    this.getPositions();
+    // this.getPositions();
   },
 
   methods: {
+    setTracking(value) {
+      if (typeof value !== "boolean") throw new Error("Value must be boolean");
+      this.isTracking = !!value;
+
+      if (this.isTracking) {
+        this.initTrack();
+      } else {
+        this.stopTrack();
+      }
+    },
+
+    async initTrack() {
+      const nord = this.NORD;
+      await axios.get(`/track/${nord}`);
+      this.getPositions();
+      alert("Track started");
+    },
+
+    async stopTrack() {
+      await axios.get("/stop-track");
+      alert("Track stoped");
+    },
+
     initMap() {
       let lat = -15.77972;
       let long = -47.92972;
@@ -51,7 +146,9 @@ export default {
     },
 
     async getPositions() {
+      console.log("GET /track-data");
       let { data, status } = await axios.get("/track-data");
+      console.log(`STATUS ${status}`);
 
       if (data.positions) {
         let { from, to } = data.positions_validation;
@@ -59,8 +156,16 @@ export default {
         MapData.from = new Date(from);
         MapData.to = new Date(to);
         MapData.positions = data.positions;
+        MapData.info = data.info;
+
+        console.log("GO marker");
 
         this.setMarker();
+      } else {
+        console.log("Não deu ?");
+        if (this.isTracking) {
+          window.setTimeout(() => this.getPositions(), 1000);
+        }
       }
     },
 
@@ -83,11 +188,14 @@ export default {
         let position = MapData.positions[curPosition++];
         let { satlatitude, satlongitude } = position;
 
+        self.current = position;
+        self.current.satname = MapData.info.satname;
+
         MapData.marker.setLatLng([satlatitude, satlongitude]);
 
         MapData.from.setSeconds(MapData.from.getSeconds() + 1);
 
-        if (MapData.from < MapData.to) {
+        if (MapData.from < MapData.to && self.isTracking) {
           window.setTimeout(tracker, 1000);
         } else {
           MapData.marker.remove();
@@ -103,18 +211,14 @@ export default {
 
 <style>
 #main-container {
-  background-color: gray;
-  border: solid 2px #000;
-  height: 99vh;
+  margin-top: 10px;
 }
 
 #controls {
-  background-color: green;
-  height: 40%;
 }
 
 #views {
-  height: 60%;
+  height: 300px;
 }
 
 #cam-container {
