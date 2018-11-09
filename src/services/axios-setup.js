30fa993a1dc1e9c3319eb4f0bebc1353597722d4
Fabio Teixeira <fabio1079@gmail.com>
2018-11-09 15:49:13 -0200

    Add DEV env docker

--- src/services/axios-setup.js
+++ src/services/axios-setup.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const BASE_URL = process.env.VUE_APP_HOST_URL;
+
+const getBaseUrl = () =>
+  BASE_URL !== undefined ? BASE_URL : "http://127.0.0.1:8000";
+
 const instance = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: `${getBaseUrl()}/api/`,
   timeout: 1000
 });
 
